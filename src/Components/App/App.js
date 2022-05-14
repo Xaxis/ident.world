c6899a67f40ce73101a6b58cf0339ad7241d88c9
Wil Neeley <wil.neeley@venafi.com>
2022-05-13 17:13:37 -0800

    Added start of basic stepper component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,24 @@
 import * as React from 'react';
-import './App.css';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import {Button, Paper} from '@mui/material';
+import BuildIdentityStepper from "../BuildIdentityStepper/BuildIdentityStepper";
 
 function App() {
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
+    // Attempt to use default system theme
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
     const theme = React.useMemo(
         () =>
             createTheme({
+                spacing: 16,
                 palette: {
                     mode: prefersDarkMode ? 'dark' : 'light',
                 },
@@ -22,16 +28,31 @@ function App() {
 
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline />
+            <CssBaseline/>
             <Container>
-                <Paper>
-                    <Typography variant="h1">
-                        Hello World
-                    </Typography>
-                    <Button>
-                        Hello Button
-                    </Button>
+                <Paper
+                    sx={{
+                        marginTop: '48px',
+                        padding: '48px'
+                    }}>
+                    <main>
+                        <Typography
+                            component={'h1'}
+                            variant={'h2'}
+                            align={'center'}
+                            color={'textPrimary'}
+                            gutterBottom
+                        >
+                            Build your identity
+                        </Typography>
+                        {/*<Typography component={'p'} variant={'h5'} align={'center'} color={'textSecondary'}>*/}
+                        {/*    Quickly build a unique identifier that securely allows you to manage your associations*/}
+                        {/*    across the web.*/}
+                        {/*</Typography>*/}
+                    </main>
+                    <BuildIdentityStepper/>
                 </Paper>
+
             </Container>
         </ThemeProvider>
     );
